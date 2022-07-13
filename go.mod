725e9c7495443946c8c41e723e30f3bccc201781
shenglei <shenglei@veer.tv>
2022-07-13 15:31:25 +0800

    md5 and logger

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/feel-easy/go-example
 go 1.16
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/apcera/termtables v0.0.0-20170405184538-bcbc5dc54055 // indirect
-	github.com/gizak/termui/v3 v3.1.0 // indirect
+	github.com/gizak/termui/v3 v3.1.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/lib/pq v1.10.5
 	github.com/mattn/go-runewidth v0.0.13 // indirect
@@ -14,7 +15,9 @@ require (
 	github.com/rs/xid v1.4.0
 	github.com/scylladb/termtables v1.0.0
 	github.com/tuotoo/qrcode v0.0.0-20220425170535-52ccc2bebf5d
+	go.uber.org/zap v1.21.0
 	google.golang.org/protobuf v1.28.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gorm.io/driver/mysql v1.3.2
 	gorm.io/gorm v1.23.3
 )
---
+++ logger/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var sugarLogger *zap.SugaredLogger
+
+func main() {
+	InitLogger()
+	defer sugarLogger.Sync()
+	for i := 0; i < 10000; i++ {
+		simpleHttpGet("www.baidu.com")
+		simpleHttpGet("http://www.baidu.com")
+	}
+
+}
+
+func InitLogger() {
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+	sugarLogger = logger.Sugar()
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+func simpleHttpGet(url string) {
+	sugarLogger.Debugf("Trying to hit GET request for %s", url)
+	// resp, err := http.Get(url)
+	err := errors.New("errr")
+	if err != nil {
+		sugarLogger.Errorf("Error fetching URL %s : Error = %s", url, err)
+	} else {
+		sugarLogger.Infof("Success! statusCode = %s for URL %s", "resp.Status", url)
+		// resp.Body.Close()
+	}
+}
